5b4256faae930b2eca7b36d2506ba7ce0c0c22af
unknown <unenes.viejos@gmail.com>
2021-01-05 08:13:55 -0600

    Creacion de los componentes (navbar y convertidor)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { ConvertidorComponent } from './componentes/convertidor/convertidor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ConvertidorComponent
   ],
   imports: [
     BrowserModule
