7212acc233ecf5bd6a4266e947efc364cc5e1bb6
unknown <unenes.viejos@gmail.com>
2021-01-06 11:38:35 -0600

    Version final lista

--- src/app/componentes/convertidor/convertidor.component.ts
+++ src/app/componentes/convertidor/convertidor.component.ts
@@ -1,3 +1,4 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -16,6 +17,68 @@ export class ConvertidorComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  convertir()
+  {
+    switch(this.tengo)
+    {
+      case 'USD':
+        // TODO
+        if(this.quiero === 'USD')
+        {
+          this.total = this.cantidad;
+        }
+
+        if(this.quiero === 'EUR')
+        {
+          this.total = parseFloat((this.cantidad * 0.81).toFixed(5));
+          
+        }
+
+        if(this.quiero === 'LIBRAS')
+        {
+          this.total = parseFloat((this.cantidad * 0.74).toFixed(5));
+        }
+        break;
+
+      case 'EUR':
+        // TODO
+
+        if(this.quiero === 'USD')
+        {
+          this.total = parseFloat((this.cantidad * 1.23).toFixed(5));
+        }
+
+        if(this.quiero === 'EUR')
+        {
+          this.total = this.cantidad;
+        }
+
+        if(this.quiero === 'LIBRAS')
+        {
+          this.total = parseFloat((this.cantidad * 0.90).toFixed(5));
+        }
+        break;
+
+      case 'LIBRAS':
+        // TODO
+        if(this.quiero === 'USD')
+        {
+          this.total = parseFloat((this.cantidad * 1.36).toFixed(5));
+        }
+
+        if(this.quiero === 'EUR')
+        {
+          this.total = parseFloat((this.cantidad * 1.11).toFixed(5));
+        }
+
+        if(this.quiero === 'LIBRAS')
+        {
+          this.total = this.cantidad;
+        }
+      break;
+    }
   }
 
 }
